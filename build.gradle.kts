4e33b0d4cf5470f6b5f7d456bd1363f8e698234f
Peter Phillips <peter@greenthistle.co.uk>
2022-09-12 14:37:31 +0100

    SDI-365: ⬆  Upgrade plugin to latest to suppress snakeyaml vuln

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.0"
   kotlin("plugin.spring") version "1.7.10"
   id("org.unbroken-dome.test-sets") version "4.0.0"
 }
@@ -33,7 +33,7 @@ dependencies {
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:4.7.0")
+  testImplementation("org.mockito:mockito-inline:4.8.0")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.2")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.testcontainers:localstack:1.17.3")
