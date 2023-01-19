1ac04c3293eaa92ec125ec80490471601579270a
petergphillips <peter@greenthistle.co.uk>
2023-01-19 16:48:15 +0000

    SDI-528: 🔒️ More suppresssions for false positives (#42)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.1"
   kotlin("plugin.spring") version "1.8.0"
   id("org.unbroken-dome.test-sets") version "4.0.0"
 }
@@ -33,8 +33,8 @@ dependencies {
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:4.11.0")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.9")
+  testImplementation("org.mockito:mockito-inline:5.0.0")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.11")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.testcontainers:localstack:1.17.6")
 }
