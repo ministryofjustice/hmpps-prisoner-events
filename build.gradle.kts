5c6bb11bd30d677555c12b427c184cd86b3931f1
Peter Phillips <peter@greenthistle.co.uk>
2023-05-22 12:48:37 +0100

    SDIT-822: ⬆️ Upgrade to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.6"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.7"
   kotlin("plugin.spring") version "1.8.21"
 }
 
@@ -7,11 +7,14 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
+// still on spring boot 2
+val hmppsSqsVersion by extra("1.3.0")
+
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("com.amazonaws:aws-java-sdk-sns")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.2.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:$hmppsSqsVersion")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
@@ -26,15 +29,15 @@ dependencies {
   implementation("com.oracle.database.messaging:aqapi:23.2.0.0")
 
   implementation("org.apache.commons:commons-lang3:3.12.0")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.1")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.13")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.14")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:localstack:1.18.0")
-  testImplementation("org.testcontainers:oracle-xe:1.18.0")
+  testImplementation("org.testcontainers:localstack:1.18.1")
+  testImplementation("org.testcontainers:oracle-xe:1.18.1")
 }
 
 java {
