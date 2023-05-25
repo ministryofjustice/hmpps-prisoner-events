324fc4b612041149ea54f8a794c28e263ded58b1
steverendell <steve.rendell@syscon.net>
2023-05-25 14:52:45 +0100

    SDIT-828 Upgrade hmpps-prisoner-events to spring boot 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.7"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.6"
   kotlin("plugin.spring") version "1.8.21"
 }
 
