4873013b6460907fe4f45eb227e7773371bbe6e5
Peter Phillips <peter@greenthistle.co.uk>
2023-06-26 10:27:35 +0100

    SDIT-893: ✨ Cache jwks indefinitely

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("org.springframework.boot:spring-boot-starter-cache")
 
   implementation("org.flywaydb:flyway-core")
 
