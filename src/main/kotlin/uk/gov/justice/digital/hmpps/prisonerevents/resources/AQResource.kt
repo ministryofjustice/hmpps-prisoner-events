f882f3494250f68465a3f1523af2c343b43a37e0
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2023-08-03 09:08:01 +0100

    SDIT-958 Drain old messages from Nomis AQ queues (#105)
    Co-authored-by: steverendell <steve.rendell@syscon.net>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerevents/resources/AQResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerevents/resources/AQResource.kt
@@ -1,16 +1,22 @@
 package uk.gov.justice.digital.hmpps.prisonerevents.resources
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.prisonerevents.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.prisonerevents.service.AQService
+import java.time.LocalDate
 
 @RestController
 @Validated
@@ -44,4 +50,40 @@ class AQResource(private val aqService: AQService) {
   )
   fun requeueExceptions() =
     aqService.requeueExceptions()
+
+  @DeleteMapping("/exceptions/{originalQueue}")
+  @PreAuthorize("hasRole('ROLE_QUEUE_ADMIN')")
+  @Operation(
+    summary = "Discards messages on the Oracle AQ exception queue",
+    description = "Dequeue messages on the Oracle AQ exception queue for the specified original queue.",
+    responses = [
+      ApiResponse(responseCode = "200", description = "success"),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [
+          Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class)),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden to access this endpoint when role not present",
+        content = [
+          Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class)),
+        ],
+      ),
+    ],
+  )
+  fun dequeueExceptions(
+    @PathVariable
+    @Schema(description = "Original queue name", example = "XTAG_UPD_OFFENDERS", required = true)
+    originalQueue: String,
+    @RequestParam
+    @Parameter(
+      description = "Only include messages enqueued before this date time",
+      example = "2021-03-23",
+      required = true,
+    )
+    onlyBefore: LocalDate,
+  ) = aqService.dequeueExceptions(originalQueue, onlyBefore)
 }
