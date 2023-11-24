9a604114c6e7a821c24a5778fab412b7765d87d0
Peter Phillips <peter@greenthistle.co.uk>
2023-11-24 10:31:09 +0000

    SDIT-1278: ⬆️ Upgrade to spring boot 3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.9.0"
-  kotlin("plugin.spring") version "1.9.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.10.0"
+  kotlin("plugin.spring") version "1.9.21"
 }
 
 configurations {
@@ -10,7 +10,7 @@ configurations {
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework:spring-jms:6.1.0")
+  implementation("org.springframework:spring-jms:6.1.1")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.1.1")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
@@ -24,7 +24,7 @@ dependencies {
   implementation("com.oracle.database.jdbc:ojdbc11:23.3.0.23.09")
   implementation("com.oracle.database.messaging:aqapi-jakarta:23.2.1.0")
 
-  implementation("org.apache.commons:commons-lang3:3.13.0")
+  implementation("org.apache.commons:commons-lang3:3.14.0")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.3")
@@ -32,13 +32,13 @@ dependencies {
 
   testImplementation("org.mockito:mockito-inline:5.2.0")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.18") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.19") {
     exclude(group = "io.swagger.core.v3")
   }
   testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.19")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:localstack:1.19.2")
-  testImplementation("org.testcontainers:oracle-xe:1.19.2")
+  testImplementation("org.testcontainers:localstack:1.19.3")
+  testImplementation("org.testcontainers:oracle-xe:1.19.3")
 }
 
 kotlin {
