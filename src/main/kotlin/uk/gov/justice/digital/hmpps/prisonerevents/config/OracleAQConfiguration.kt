91d853267c2a7e3d8f5522b04892f0b702814050
andymarke <andy.marke@digital.justice.gov.uk>
2024-07-31 11:38:57 +0100

    SDIT-1820 Use hikari pooling for jdbctemplate lookups

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerevents/config/OracleAQConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerevents/config/OracleAQConfiguration.kt
@@ -1,42 +1,51 @@
 package uk.gov.justice.digital.hmpps.prisonerevents.config
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import jakarta.jms.QueueConnectionFactory
 import oracle.jakarta.jms.AQjmsFactory
 import oracle.jdbc.datasource.impl.OracleDataSource
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.jms.connection.CachingConnectionFactory
 import javax.sql.DataSource
 
 @Configuration
 class OracleAQConfiguration {
-  @Value("\${spring.datasource.username:dummy}")
-  private lateinit var user: String
-
-  @Value("\${spring.datasource.password:dummy}")
-  private lateinit var password: String
-
-  @Value("\${spring.datasource.url}")
-  private lateinit var connectionstring: String
-
   // see https://docs.oracle.com/en/database/oracle/oracle-database/21/jajdb/oracle/jdbc/OracleConnection.html
-
   @Bean
-  fun dataSource(): DataSource {
-    val ds = OracleDataSource()
-    ds.user = user
-    ds.setPassword(password)
-    ds.url = connectionstring
-    ds.setConnectionProperty("autocommit", "false")
-    return ds
+  fun listenerDataSource(dataSourceProperties: DataSourceProperties): DataSource = OracleDataSource().apply {
+    this.url = dataSourceProperties.url
+    this.user = dataSourceProperties.username
+    this.setPassword(dataSourceProperties.password)
+    this.setConnectionProperty("autocommit", "false")
   }
 
   @Bean
-  fun listenerConnectionFactory(dataSource: DataSource): QueueConnectionFactory =
+  @Primary
+  fun dataSource(dataSourceProperties: DataSourceProperties, poolConfig: HikariPoolConfiguration): DataSource = HikariDataSource(
+    // since the data source is not auto-configured we need to set the hikari properties manually from config and set
+    // the data source properties as well
+    poolConfig.apply {
+      this.jdbcUrl = dataSourceProperties.url
+      this.username = dataSourceProperties.username
+      this.password = dataSourceProperties.password
+    },
+  )
+
+  @Configuration
+  @ConfigurationProperties("spring.datasource.hikari")
+  class HikariPoolConfiguration : HikariConfig()
+
+  @Bean
+  fun listenerConnectionFactory(@Qualifier("listenerDataSource") dataSource: DataSource): QueueConnectionFactory =
     AQjmsFactory.getQueueConnectionFactory(dataSource) // Should not cache this connection factory according to Spring documentation
 
   @Bean
-  fun retryConnectionFactory(dataSource: DataSource): QueueConnectionFactory =
+  fun retryConnectionFactory(@Qualifier("listenerDataSource") dataSource: DataSource): QueueConnectionFactory =
     CachingConnectionFactory(AQjmsFactory.getQueueConnectionFactory(dataSource)) // Should cache this connection factory
 }
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerevents/integration/OraclePoolingIntTest.kt
@@ -0,0 +1,152 @@
+package uk.gov.justice.digital.hmpps.prisonerevents.integration
+
+import com.zaxxer.hikari.HikariDataSource
+import jakarta.jms.ConnectionFactory
+import jakarta.jms.MapMessage
+import jakarta.jms.MessageProducer
+import jakarta.jms.Session
+import oracle.jdbc.datasource.impl.OracleDataSource
+import org.assertj.core.api.Assertions.assertThat
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.matches
+import org.awaitility.kotlin.untilCallTo
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.reset
+import org.mockito.kotlin.spy
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.test.mock.mockito.SpyBean
+import org.springframework.jms.core.JmsTemplate
+import org.springframework.jms.core.ProducerCallback
+import software.amazon.awssdk.services.sns.SnsAsyncClient
+import software.amazon.awssdk.services.sqs.model.PurgeQueueRequest
+import uk.gov.justice.digital.hmpps.prisonerevents.config.FULL_QUEUE_NAME
+import uk.gov.justice.digital.hmpps.prisonerevents.repository.SqlRepository
+import uk.gov.justice.hmpps.sqs.HmppsQueue
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+import uk.gov.justice.hmpps.sqs.HmppsTopic
+import uk.gov.justice.hmpps.sqs.countAllMessagesOnQueue
+import java.time.Duration
+import javax.sql.DataSource
+
+@SpringBootTest(
+  webEnvironment = RANDOM_PORT,
+  properties = [
+    "jms.connection.concurrentConsumers=3",
+    "jms.connection.maxConcurrentConsumers=3",
+    "spring.datasource.hikari.maximum-pool-size=4",
+  ],
+)
+class OraclePoolingIntTest : IntegrationTestBase() {
+
+  @SpyBean
+  private lateinit var hmppsQueueService: HmppsQueueService
+
+  @SpyBean
+  @Qualifier("listenerDataSource")
+  private lateinit var listenerDataSource: DataSource
+
+  @SpyBean
+  private lateinit var jdbcDataSource: DataSource
+
+  @Autowired
+  private lateinit var retryConnectionFactory: ConnectionFactory
+
+  @Autowired
+  private lateinit var sqlRepository: SqlRepository
+
+  private val jmsTemplate by lazy {
+    JmsTemplate(retryConnectionFactory).also { it.defaultDestinationName = FULL_QUEUE_NAME }
+  }
+
+  internal val prisonEventQueue by lazy { hmppsQueueService.findByQueueId("prisoneventtestqueue") as HmppsQueue }
+  internal val prisonEventQueueSqsClient by lazy { prisonEventQueue.sqsClient }
+  internal val prisonEventQueueUrl by lazy { prisonEventQueue.queueUrl }
+
+  companion object {
+    private var isTopicSpySetUp = false
+    private lateinit var snsClient: SnsAsyncClient
+  }
+
+  @BeforeEach
+  fun setup() {
+    if (!isTopicSpySetUp) {
+      val realTopic = hmppsQueueService.findByTopicId("prisoneventtopic")!!
+      val realSnsClient = realTopic.snsClient
+      snsClient = spy(realSnsClient)
+      val mockTopic = HmppsTopic(arn = realTopic.arn, snsClient = snsClient, id = "prisoneventtopic")
+      whenever(hmppsQueueService.findByTopicId("prisoneventtopic")).thenReturn(mockTopic)
+      isTopicSpySetUp = true
+    }
+    // Ensure publish is called normally and call logging is reset
+    reset(snsClient)
+    purgeQueues()
+  }
+
+  @Nested
+  inner class Consume {
+
+    // This is best efforts to confirm connection factories are setup correctly and connection pooling
+    // is being used for jdbcTemplate but nt by the listeners
+    // Ideally we woul want to look at how many real Oracle connections have been created, but not sure of a good
+    // way to do this in an integration test.
+    @Test
+    fun `will consume all prison offender events message with pooled jdbc connection and connection per listener thread`() {
+      val count = 60
+      simulateBatchTrigger(count) {
+        it.createMapMessage().apply {
+          jmsType = "OFF_RECEP_OASYS"
+          setLong("p_offender_book_id", 1234567L)
+          setInt("p_movement_seq", 4)
+          setString("eventType", "OFF_RECEP_OASYS")
+        }
+      }
+
+      awaitQueueSizeToBe(count)
+
+      // NB The number of connections should match messages and threads buyt will not be exact due to health check beans
+      // and test beans being created
+      // connection for pooled jdbc connection called as many times as there is message plus 2 other beans
+      // but check it is a Hikari pool so we trust it uses a pooled connection
+      assertThat(jdbcDataSource is HikariDataSource).isTrue()
+      verify(jdbcDataSource, times(62)).connection
+      // connection for each of 3 listeners plus 2 jms templates
+      assertThat(listenerDataSource is OracleDataSource).isTrue()
+      verify(listenerDataSource, times(5)).connection
+    }
+  }
+
+  private fun simulateBatchTrigger(count: Int, messageCreator: (session: Session) -> MapMessage) {
+    val producerCallback: ProducerCallback<*> = ProducerCallback { session: Session, producer: MessageProducer ->
+      (1..count).forEach { _ ->
+        producer.send(messageCreator(session))
+      }
+    }
+    jmsTemplate.execute(producerCallback)
+  }
+
+  fun purgeQueues() {
+    prisonEventQueueSqsClient.purgeQueue(PurgeQueueRequest.builder().queueUrl(prisonEventQueueUrl).build())
+    sqlRepository.purgeExceptionQueue()
+    awaitQueueSizeToBe(0)
+  }
+
+  val maxWait: Duration = Duration.ofSeconds(30)
+  val poll: Duration = Duration.ofSeconds(1)
+
+  fun awaitQueueSizeToBe(count: Int) {
+    await.atMost(maxWait)
+      .pollInterval(poll) untilCallTo { getNumberOfMessagesCurrentlyOnPrisonEventQueue() } matches { it == count }
+  }
+
+  fun getNumberOfMessagesCurrentlyOnPrisonEventQueue(): Int =
+    prisonEventQueueSqsClient.countAllMessagesOnQueue(prisonEventQueueUrl).get()
+      .also { log.info("Number of messages on prison queue: $it") }
+}
