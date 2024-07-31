5c09e96878f217be3143484746ac06b36a254e0e
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-07-31 14:43:25 +0100

    SDIT-1820 Use hikari pooling for jdbctemplate lookups (#216)
    * SDIT-1820 Use hikari pooling for jdbctemplate lookups
    
    * SDIT-1820 Use hikari pooling for jdbctemplate lookups
    
    Debug
    
    * SDIT-1820 Use hikari pooling for jdbctemplate lookups
    When running test do not get connection on Kikari pool creation so that Flyway can run first
    
    * SDIT-1820 Use hikari pooling for jdbctemplate lookups
    When running test do not get connection on Kikari pool creation so that Flyway can run first

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
 
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 
+  implementation("com.zaxxer:HikariCP:5.1.0")
   implementation("com.oracle.database.jdbc:ojdbc11:23.4.0.24.05")
   implementation("com.oracle.database.messaging:aqapi-jakarta:23.3.1.0")
 
