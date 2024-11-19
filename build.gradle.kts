7bff8024c12e510340e8f0166b7161d7fd1d0d4a
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-11-19 14:53:33 +0000

    SDIT-2265: ⬆️  Upgrade spring boot plugin for netty upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.9"
   kotlin("plugin.spring") version "2.0.21"
 }
 
@@ -11,7 +11,7 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.8")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework:spring-jms:6.1.14")
+  implementation("org.springframework:spring-jms:6.2.0")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.1.0")
 
   implementation("org.flywaydb:flyway-database-oracle")
@@ -20,23 +20,23 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 
   runtimeOnly("com.zaxxer:HikariCP")
-  implementation("com.oracle.database.jdbc:ojdbc11:23.5.0.24.07")
+  implementation("com.oracle.database.jdbc:ojdbc11:23.6.0.24.10")
   implementation("com.oracle.database.messaging:aqapi-jakarta:23.3.1.0")
 
   implementation("org.apache.commons:commons-lang3:3.17.0")
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.9.0")
-  implementation("org.jetbrains.exposed:exposed-spring-boot-starter:0.55.0")
-  implementation("org.jetbrains.exposed:exposed-java-time:0.55.0")
+  implementation("org.jetbrains.exposed:exposed-spring-boot-starter:0.56.0")
+  implementation("org.jetbrains.exposed:exposed-java-time:0.56.0")
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.8")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
 
   testImplementation("org.mockito:mockito-inline:5.2.0")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.23") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.24") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.25")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.26")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.testcontainers:localstack:1.20.3")
   testImplementation("org.testcontainers:oracle-xe:1.20.3")
