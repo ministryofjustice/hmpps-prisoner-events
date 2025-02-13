a4b07a2c13b8f64bb999a3671344c9b55f215705
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-02-13 15:22:27 +0000

    SDIT-2561: ⬆️  Upgrade hmpps spring boot plugin to 7.7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.2"
   kotlin("plugin.spring") version "2.1.10"
 }
 
@@ -11,7 +11,7 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.2.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework:spring-jms:6.2.2")
+  implementation("org.springframework:spring-jms:6.2.3")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.0")
 
   implementation("org.flywaydb:flyway-database-oracle")
