92c01ea0d02880b15ffa5bee343839cccd7fd285
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-03-12 14:29:06 +0000

    SDIT-2654: ⬆️  Upgrade hmpps spring boot plugin to 7.1.4 for swagger-ui CVE (#300)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.4"
   kotlin("plugin.spring") version "2.1.10"
 }
 
@@ -8,11 +8,11 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.0-beta")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework:spring-jms:6.2.3")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.2")
 
   implementation("org.flywaydb:flyway-database-oracle")
   implementation("org.hibernate.orm:hibernate-community-dialects")
@@ -25,10 +25,10 @@ dependencies {
 
   implementation("org.apache.commons:commons-lang3:3.17.0")
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.12.0")
-  implementation("org.jetbrains.exposed:exposed-spring-boot-starter:0.59.0")
-  implementation("org.jetbrains.exposed:exposed-java-time:0.59.0")
+  implementation("org.jetbrains.exposed:exposed-spring-boot-starter:0.60.0")
+  implementation("org.jetbrains.exposed:exposed-java-time:0.60.0")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.3.1")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
 
   testImplementation("org.mockito:mockito-inline:5.2.0")
@@ -36,10 +36,10 @@ dependencies {
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.25") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.28")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.29")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:localstack:1.20.5")
-  testImplementation("org.testcontainers:oracle-xe:1.20.5")
+  testImplementation("org.testcontainers:localstack:1.20.6")
+  testImplementation("org.testcontainers:oracle-xe:1.20.6")
 }
 
 kotlin {
