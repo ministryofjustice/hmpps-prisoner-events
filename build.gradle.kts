45bc5d2c047552f8fda96e910697cccbd6cd31a7
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-05-23 16:38:28 +0100

    SDIT-2788: ⬆️  Upgrade hmpps spring boot plugin to 8.2.0 (#314)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.1.0"
-  kotlin("plugin.spring") version "2.1.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.2.0"
+  kotlin("plugin.spring") version "2.1.21"
 }
 
 configurations {
@@ -8,19 +8,19 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.3")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.5")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework:spring-jms:6.2.6")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.4")
+  implementation("org.springframework:spring-jms:6.2.7")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.5")
 
   implementation("org.flywaydb:flyway-database-oracle")
   implementation("org.hibernate.orm:hibernate-community-dialects")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
 
   runtimeOnly("com.zaxxer:HikariCP")
-  implementation("com.oracle.database.jdbc:ojdbc11:23.7.0.25.01")
+  implementation("com.oracle.database.jdbc:ojdbc11:23.8.0.25.04")
   implementation("com.oracle.database.messaging:aqapi-jakarta:23.7.0.0")
 
   implementation("org.apache.commons:commons-lang3:3.17.0")
@@ -28,15 +28,15 @@ dependencies {
   implementation("org.jetbrains.exposed:exposed-spring-boot-starter:0.61.0")
   implementation("org.jetbrains.exposed:exposed-java-time:0.61.0")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.3")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.5")
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
 
   testImplementation("org.mockito:mockito-inline:5.2.0")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.26") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.28") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.30")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.32")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.testcontainers:localstack:1.21.0")
   testImplementation("org.testcontainers:oracle-xe:1.21.0")
