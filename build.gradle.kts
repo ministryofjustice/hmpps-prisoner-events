bacd49fe99298a0fc387cf03d277f1e51655362e
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-08-12 08:21:54 +0100

    SDIT-2094 Use client credentials timeout on full version (#347)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.5.0-beta")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.5.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework:spring-jms:6.2.9")
