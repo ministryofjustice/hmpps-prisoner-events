755220063a8184ebcbe133bd07c3470249ed9d95
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-01-13 09:45:10 +0000

    SDIT-3303: ✅ Add test to ensure correct openapi generation

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerevents/integration/ZzOpenApiDocsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerevents/integration/ZzOpenApiDocsTest.kt
@@ -3,8 +3,10 @@ package uk.gov.justice.digital.hmpps.prisonerevents.integration
 import io.swagger.v3.parser.OpenAPIV3Parser
 import net.minidev.json.JSONArray
 import org.assertj.core.api.Assertions.assertThat
-import org.hamcrest.Matchers.containsString
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.MediaType
 import java.time.LocalDate
@@ -40,8 +42,14 @@ class ZzOpenApiDocsTest : IntegrationTestBase() {
       .accept(MediaType.APPLICATION_JSON)
       .exchange()
       .expectStatus().isOk
-      .expectBody()
-      .jsonPath("paths").isNotEmpty
+      .expectBody().jsonPath("paths").isNotEmpty
+  }
+
+  @Test
+  fun `the open api json is valid and contains documentation`() {
+    val result = OpenAPIV3Parser().readLocation("http://localhost:$port/v3/api-docs", null, null)
+    assertThat(result.messages).isEmpty()
+    assertThat(result.openAPI.paths).isNotEmpty
   }
 
   @Test
@@ -57,25 +65,60 @@ class ZzOpenApiDocsTest : IntegrationTestBase() {
   }
 
   @Test
-  fun `the open api json is valid and contains documentation`() {
-    val result = OpenAPIV3Parser().readLocation("http://localhost:$port/v3/api-docs", null, null)
-    assertThat(result.messages).isEmpty()
-    assertThat(result.openAPI.paths).isNotEmpty
-  }
-
-  @Test
-  fun `a security scheme is setup for a HMPPS Auth token with the ROLE_QUEUE_ADMIN role`() {
+  @Disabled("Enable this test if any LocalDateTime fields are added")
+  fun `the generated open api for date times hasn't got the time zone`() {
     webTestClient.get()
       .uri("/v3/api-docs")
       .accept(MediaType.APPLICATION_JSON)
       .exchange()
       .expectStatus().isOk
       .expectBody()
-      .jsonPath("$.components.securitySchemes.queue-admin-role.type").isEqualTo("http")
-      .jsonPath("$.components.securitySchemes.queue-admin-role.scheme").isEqualTo("bearer")
-      .jsonPath("$.components.securitySchemes.queue-admin-role.bearerFormat").isEqualTo("JWT")
-      .jsonPath("$.components.securitySchemes.queue-admin-role.description").value(containsString("ROLE_QUEUE_ADMIN"))
-      .jsonPath("$.security[0].queue-admin-role")
-      .isEqualTo(JSONArray().apply { addAll(listOf("read", "write")) })
+      .jsonPath("$.components.schemas.ProfileDetailsResponse.properties.modifiedDateTime.example").isEqualTo("2021-07-16T12:34:56")
+      .jsonPath("$.components.schemas.ProfileDetailsResponse.properties.modifiedDateTime.description")
+      .isEqualTo("The time the profile info was last changed")
+      .jsonPath("$.components.schemas.ProfileDetailsResponse.properties.modifiedDateTime.type").isEqualTo("string")
+      .jsonPath("$.components.schemas.ProfileDetailsResponse.properties.modifiedDateTime.format").isEqualTo("date-time")
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = ["queue-admin-role, ROLE_QUEUE_ADMIN"])
+  fun `the security scheme is setup for bearer tokens`(key: String, role: String) {
+    webTestClient.get()
+      .uri("/v3/api-docs")
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isOk
+      .expectBody()
+      .jsonPath("$.components.securitySchemes.$key.type").isEqualTo("http")
+      .jsonPath("$.components.securitySchemes.$key.scheme").isEqualTo("bearer")
+      .jsonPath("$.components.securitySchemes.$key.description").value<String> {
+        assertThat(it).contains(role)
+      }
+      .jsonPath("$.components.securitySchemes.$key.bearerFormat").isEqualTo("JWT")
+      .jsonPath("$.security[0].$key").isEqualTo(JSONArray().apply { addAll(listOf("read", "write")) })
+  }
+
+  @Test
+  fun `the open api json doesn't include LocalTime`() {
+    webTestClient.get()
+      .uri("/v3/api-docs")
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isOk
+      .expectBody()
+      .jsonPath("components.schemas.LocalTime").doesNotExist()
+  }
+
+  @Test
+  fun `the response contains required fields`() {
+    webTestClient.get()
+      .uri("/v3/api-docs")
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isOk
+      .expectBody()
+      .jsonPath("$.components.schemas.ErrorResponse.required").value<List<String>> {
+        assertThat(it).containsExactly("status")
+      }
   }
 }
