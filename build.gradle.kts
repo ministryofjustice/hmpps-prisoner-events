1c0e14e46abb029ef0f4a93b7e958088bf706c58
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-03-19 19:35:54 +0000

    SDIT-3603: ⬆️ Upgrade to spring boot 4.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.6"
-  kotlin("plugin.spring") version "2.3.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.1.0"
+  kotlin("plugin.spring") version "2.3.20"
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.1.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework:spring-jms:7.0.5")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.0.1")
+  implementation("org.springframework:spring-jms:7.0.6")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.2.0")
   implementation("org.springframework.boot:spring-boot-starter-flyway")
 
   implementation("org.flywaydb:flyway-database-oracle")
-  implementation("org.hibernate.orm:hibernate-community-dialects:7.2.6.Final")
+  implementation("org.hibernate.orm:hibernate-community-dialects:7.2.7.Final")
 
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
 
@@ -28,7 +28,7 @@ dependencies {
   implementation("org.jetbrains.exposed:exposed-spring-boot4-starter:1.1.1")
   implementation("org.jetbrains.exposed:exposed-java-time:1.1.1")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.0.2")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.1.0")
   testImplementation("org.springframework.boot:spring-boot-test-autoconfigure")
   testImplementation("org.springframework.boot:spring-boot-starter-jdbc-test")
   testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
