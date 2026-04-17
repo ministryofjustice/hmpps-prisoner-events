4adfc8576ae804b9e23db36ab49cf346dcaea1a2
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-04-17 11:18:31 +0100

    SDIT-3687: 🔒️ Upgrade tomcat for security issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.1.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.2.1"
   kotlin("plugin.spring") version "2.3.20"
 }
 
@@ -7,7 +7,7 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.1.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework:spring-jms:7.0.6")
+  implementation("org.springframework:spring-jms:7.0.7")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.3.0")
   implementation("org.springframework.boot:spring-boot-starter-flyway")
 
@@ -15,18 +15,21 @@ dependencies {
   implementation("org.hibernate.orm:hibernate-community-dialects")
 
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+  constraints {
+    implementation("org.webjars:swagger-ui:5.32.2")
+  }
 
   runtimeOnly("com.zaxxer:HikariCP")
   runtimeOnly("com.oracle.database.jdbc:ojdbc11:23.26.1.0.0")
-  implementation("com.oracle.database.messaging:aqapi-jakarta:23.8.0.0")
+  implementation("com.oracle.database.messaging:aqapi-jakarta:23.9.0.0")
 
   implementation("org.apache.commons:commons-lang3:3.20.0")
   // Needs to match this version https://github.com/microsoft/ApplicationInsights-Java/blob/<version>/dependencyManagement/build.gradle.kts#L16
   // where <version> is the version of application insights pulled in by hmpps-gradle-spring-boot
   // at https://github.com/ministryofjustice/hmpps-gradle-spring-boot/blob/main/src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/configmanagers/AppInsightsConfigManager.kt#L7
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.26.1")
-  implementation("org.jetbrains.exposed:exposed-spring-boot4-starter:1.1.1")
-  implementation("org.jetbrains.exposed:exposed-java-time:1.1.1")
+  implementation("org.jetbrains.exposed:exposed-spring-boot4-starter:1.2.0")
+  implementation("org.jetbrains.exposed:exposed-java-time:1.2.0")
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.1.0")
   testImplementation("org.springframework.boot:spring-boot-test-autoconfigure")
