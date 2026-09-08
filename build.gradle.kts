d57cbd5b4d5979d78e5d6f07fbbe0fd27742f44b
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-09-08 07:53:21 +0100

    SDIT-4203: ⬆️  Spring doc and other upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.7"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.8"
   kotlin("plugin.spring") version "2.4.10"
 }
 
@@ -14,7 +14,7 @@ dependencies {
   implementation("org.flywaydb:flyway-database-oracle")
   implementation("org.hibernate.orm:hibernate-community-dialects")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.1")
 
   runtimeOnly("com.zaxxer:HikariCP")
   // Ensure that the oracle version doesn't automatically get updated
